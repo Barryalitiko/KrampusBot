6496c52ffc02cccea617589339d54ebd4d0a1f4b
Krampus B. <tiobarryx@gmail.com>
2025-01-01 10:44:25 +0100

    Actualizar play-audio.js

--- src/commands/member/play-audio.js
+++ src/commands/member/play-audio.js
@@ -14,27 +14,39 @@ module.exports = {
     sendSuccessReact,
     sendErrorReply,
   }) => {
+    console.log("Comando 'play-audio' iniciado.");
+    console.log("Argumentos recibidos:", args);
+
     if (!args.length) {
+      console.error("Error: No se proporcionaron argumentos.");
       throw new InvalidParameterError(
         "Você precisa me dizer o que deseja buscar!"
       );
     }
 
+    console.log("Enviando reacción de espera...");
     await sendWaitReact();
 
     try {
+      console.log("Llamando a la función playAudio con argumento:", args[0]);
       const data = await playAudio(args[0]);
+      console.log("Respuesta recibida de playAudio:", data);
 
       if (!data) {
+        console.warn("Advertencia: No se encontraron resultados.");
         await sendErrorReply("Nenhum resultado encontrado!");
         return;
       }
 
+      console.log("Enviando reacción de éxito...");
       await sendSuccessReact();
 
+      console.log("Enviando audio desde URL:", data.url);
       await sendAudioFromURL(data.url);
     } catch (error) {
-      console.log(error);
+      console.error("Error ocurrido:", error);
+      console.log("Mensaje del error:", error.message);
+
       await sendErrorReply(error.message);
     }
   },
